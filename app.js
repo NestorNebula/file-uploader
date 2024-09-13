690ee4a7aa7cb59b97ed60ad2207515a893ec398
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-13 18:18:44 +0200

    feat: set app routes with routers and controllers

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('node:path');
 require('dotenv').config();
+const signupRouter = require('./routes/signupRouter');
+const loginRouter = require('./routes/loginRouter');
+const newfileRouter = require('./routes/newfileRouter');
+const indexController = require('./controllers/indexController');
+const userController = require('./controllers/userController');
+const fileController = require('./controllers/fileController');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', indexController);
+app.use('/sign-up', signupRouter);
+app.use('/log-in', loginRouter);
+app.get('/user', userController);
+app.get('/user/:folder/:file', fileController);
+app.use('/new-file', newfileRouter);
 
 app.listen(process.env.PORT);
