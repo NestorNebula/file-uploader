fc30b641f9c3ffa93d63243a8d771c24e00f1a80
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-14 11:00:15 +0200

    feat: add signup controller to router

--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
 const signupRouter = Router();
+const signupController = require('../controllers/signupController');
+
+signupRouter.get('/', signupController.getSignup);
+signupRouter.post('/', signupController.postSignup);
 
 module.exports = signupRouter;
