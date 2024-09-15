e7fadadb2423e877df78224d6845e298e080ad19
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-15 18:01:46 +0200

    feat: add getNew and postNew (folder)

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,5 +1,21 @@
-const getNew = () => {};
+const { body } = require('express-validator');
+const prisma = require('../models/queries');
 
-const postNew = () => {};
+const validateFolder = [body('fname').trim().blacklist('<>')];
+
+const getNew = (req, res) => {
+  res.render('new');
+};
+
+const postNew = [
+  validateFolder,
+  (req, res) => {
+    if (req.body.action_type === 'cfolder') {
+      prisma.createFolder({ name: req.body.fname, user: req.user });
+      return res.redirect('/');
+    }
+    res.redirect('/');
+  },
+];
 
 module.exports = { getNew, postNew };
