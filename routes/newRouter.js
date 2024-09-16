8aa95a0f79db45ce7b1c8c71553a54e16f001e0d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-16 18:25:18 +0200

    feat: add multer in newRouter

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const newRouter = Router();
 const newController = require('../controllers/newController');
+const multer = require('multer');
+const upload = multer({ dest: './public/uploads/' });
 
 newRouter.get('/', newController.getNew);
-newRouter.post('/', newController.postNew);
+newRouter.post('/', upload.single('ufile'), newController.postNew);
 
 module.exports = newRouter;
