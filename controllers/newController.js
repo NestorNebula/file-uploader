8e39b4fbac692e8aab599f363659154fcf314ef7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-17 11:20:58 +0200

    feat: create file and store it with cloudinary

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,7 +1,12 @@
 const { body } = require('express-validator');
 const prisma = require('../models/queries');
+const cloudinary = require('../modules/cloudinary');
 
 const validateFolder = [body('fname').trim().blacklist('<>')];
+
+const setOptions = (id) => {
+  return { public_id: id, overwrite: true };
+};
 
 const getNew = async (req, res) => {
   const folders = await prisma.getAllFolders();
@@ -13,7 +18,18 @@ const postNew = [
   async (req, res) => {
     if (req.body.action_type === 'cfolder') {
       await prisma.createFolder({ name: req.body.fname, user: req.user });
-      return res.redirect('/');
+    } else if (req.body.action_type === 'cfile') {
+      const id = req.file.originalname.split('.')[0] + req.user.id;
+      const options = setOptions(id);
+      const b64 = Buffer.from(req.file.buffer).toString('base64');
+      let dataUri = 'data:' + req.file.mimetype + ';base64,' + b64;
+      const result = await cloudinary.uploader.upload(dataUri, options);
+      await prisma.createFile({
+        url: result.secure_url,
+        name: req.body.fname,
+        size: req.file.size,
+        folder: +req.body.folder,
+      });
     }
     res.redirect('/');
   },
