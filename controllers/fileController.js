c25d7e857bd5de55db786aeac9810e1e1be00a6f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-17 16:24:42 +0200

    feat: add getFile function

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,3 +1,14 @@
-const getFile = () => {};
+const prisma = require('../models/queries');
+
+const getFile = (req, res) => {
+  const file = prisma.getFile(+req.params.file);
+  const folder = prisma.getFolderById(+req.params.folder);
+  Promise.all([file, folder]).then((values) => {
+    if (values[1].userId !== req.user.id) {
+      throw new Error('Unauthorized to access the file.');
+    }
+    return res.render('file', { file: values[0] });
+  });
+};
 
 module.exports = getFile;
