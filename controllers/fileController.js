701c0a1ae82e465037598ffe9fc1631ea3353cd7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-17 18:20:09 +0200

    feat: add postFile function to download a file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,4 +1,6 @@
 const prisma = require('../models/queries');
+const { createWriteStream, unlink } = require('fs');
+const { Readable } = require('stream');
 
 const getFile = (req, res) => {
   const file = prisma.getFile(+req.params.file);
@@ -7,8 +9,24 @@ const getFile = (req, res) => {
     if (values[1].userId !== req.user.id) {
       throw new Error('Unauthorized to access the file.');
     }
-    return res.render('file', { file: values[0] });
+    return res.render('file', { file: values[0], folder: values[1] });
   });
 };
 
-module.exports = getFile;
+const postFile = async (req, res) => {
+  const filePath = './public/' + req.body.url.split('/').pop();
+  const file = await fetch(req.body.url, { mode: 'cors' });
+  let writer = createWriteStream(filePath);
+  Readable.fromWeb(file.body).pipe(writer);
+  writer.on('finish', () => {
+    writer.close();
+    res.download(filePath, 'secondtest.png', (err) => {
+      if (err) console.err(err);
+      unlink(filePath, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+};
+
+module.exports = { getFile, postFile };
