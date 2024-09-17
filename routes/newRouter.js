c10d09bda700f8f210fdfcec7f926e221984bbe1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-17 09:34:34 +0200

    feat: use multer's memory storage

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 const newRouter = Router();
 const newController = require('../controllers/newController');
 const multer = require('multer');
-const upload = multer({ dest: './public/uploads/' });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 newRouter.get('/', newController.getNew);
 newRouter.post('/', upload.single('ufile'), newController.postNew);
