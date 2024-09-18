08d56fa3029fda54e894ac667dd9776c9eb8dc93
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-18 11:01:51 +0200

    feat: add postShare function

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -1,5 +1,20 @@
+const prisma = require('../models/queries');
+
 const getShare = () => {};
 
-const postShare = () => {};
+const postShare = async (req, res) => {
+  const date = new Date();
+  req.body.exDate === 'hours'
+    ? date.setDate(date.getDate() + 1)
+    : req.body.exDate === 'days'
+    ? date.setDate(date.getDate() + 10)
+    : date.setMonth(date.getMonth() + 1);
+  const existingLink = await prisma.getLinkByFolderId(+req.body.folder);
+  if (existingLink) {
+    await prisma.updateLink(date, +req.body.folder);
+  }
+  await prisma.createLink(date, +req.body.folder);
+  res.redirect('/user');
+};
 
 module.exports = { getShare, postShare };
