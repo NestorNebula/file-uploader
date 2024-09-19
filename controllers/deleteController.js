2d26c7c950cb79c1533b9ea300bf18bfd703bdc3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-19 12:07:52 +0200

    feat: delete cloudinary files on folder delete

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,10 +1,15 @@
 const prisma = require('../models/queries');
+const cloudinary = require('../modules/cloudinary');
 
 const postDelete = async (req, res) => {
   const folder = await prisma.getFolderById(+req.body.id);
   if (!folder || req.user.id !== folder.userId) {
     return res.redirect('/user');
   }
+  const files = folder.Files.map((file) =>
+    file.url.split('/').pop().split('.').shift()
+  );
+  cloudinary.api.delete_resources(files);
   await prisma.deleteFolder(+req.body.id);
   res.redirect('/user');
 };
