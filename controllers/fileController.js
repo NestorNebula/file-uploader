bafa2b0784a6ea7ed5af86aa3e1e1a588fb44ea5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-19 12:27:27 +0200

    feat: add form and route to delete file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,5 +1,6 @@
 const prisma = require('../models/queries');
 const download = require('../modules/download');
+const cloudinary = require('../modules/cloudinary');
 
 const getFile = (req, res) => {
   const file = prisma.getFile(+req.params.file);
@@ -16,4 +17,13 @@ const postFile = async (req, res) => {
   download(req.body.url, res);
 };
 
-module.exports = { getFile, postFile };
+const postDeleteFile = async (req, res) => {
+  const file = await prisma.getFile(+req.params.file);
+  if (!file) return res.redirect('/');
+  const fileName = file.url.split('/').pop().split('.').shift();
+  cloudinary.api.delete_resources([fileName]);
+  await prisma.deleteFile(+req.params.file);
+  res.redirect('/user');
+};
+
+module.exports = { getFile, postFile, postDeleteFile };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,6 @@ const fileController = require('../controllers/fileController');
 userRouter.get('/', userController);
 userRouter.get('/:folder/:file', fileController.getFile);
 userRouter.post('/:folder/:file', fileController.postFile);
+userRouter.post('/:folder/:file/delete', fileController.postDeleteFile);
 
 module.exports = userRouter;
